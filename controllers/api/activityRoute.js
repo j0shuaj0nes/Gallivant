73ff4f59c0fc8950a9037e453934cdbcdcaa20e0
Diana Tran <diana.tran44@gmail.com>
2024-02-05 22:38:54 +1100

    updated css

--- controllers/api/activityRoute.js
+++ controllers/api/activityRoute.js
@@ -1,42 +1,36 @@
 const router = require('express').Router();
-const tempCity = 'Barcelona';
-const {City} = require('../../models');
+const City = require('../../models/City');
 const Amadeus = require('amadeus')
 
-
-router.get('/', async (req, res) => {
-  //console.log('Accessed the activities route');
+router.get('/:tempCity', async (req, res) => {
+  //   console.log('Accessed the activity route');
   try {
     const cityData =  await City.findOne({
       where: {
-        name: tempCity
+        name: req.params.tempCity 
       }
     })
-  //console.log(cityData);
-    
+  
   const amadeusData =  new Amadeus({
       clientId: process.env.CLIENT_ID, 
       clientSecret: process.env.CLIENT_SECRET,
     })
-    // console.log(cityData);
-
+   
    const amadeusReturn =  await amadeusData.shopping.activities.bySquare.get({
-      north: 41.397158,
-      west: 2.160873, 
-      south: 41.394582,
-      east: 2.177181
+      north: 48.91,
+      west: 2.25, 
+      south: 48.80,
+      east: 2.46
     })
 
     res.json(JSON.parse(amadeusReturn.body))
-    console.log(amadeusReturn.body);
+    // console.log(amadeusReturn.body);
     
-    //  // Render the Handlebars view with the JSON data
-    //  res.render(activities, { activities: jsonData });
+
 
   } catch (error) {
     console.error('Error making API request:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-    res.status(404).json({ error: 'City not found' });
+    res.status(500).json({ error: 'Internal Server Error' })
   }
 });
 
