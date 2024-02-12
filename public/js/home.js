92e9d086922be7ba98707e34fdc1b11fdac4b2d4
dtran44 <146193635+dtran44@users.noreply.github.com>
2024-02-12 19:57:41 +1100

    Update home.js comments

--- public/js/home.js
+++ public/js/home.js
@@ -2,6 +2,7 @@ const dropdownButton = document.querySelector('.dropdown-button');
 const citySelect = document.querySelector('.dropdown-options');
 const citySelectOptions = document.querySelectorAll('.dropdown-option');
 
+//Event listener for the city selected by the user
 dropdownButton.addEventListener('click', () => {
   citySelect.style.display = citySelect.style.display === 'none' ? 'block' : 'none';
 });
@@ -11,6 +12,7 @@ citySelectOptions.forEach(option => {
     const selectedCity = option.innerText;
     console.log("Selected City:", selectedCity);
 
+//the selected city is saved to local storage
     if (selectedCity) {
       localStorage.setItem("savedCity",selectedCity)
       document.location.replace('/preference');
