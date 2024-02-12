70347e808b81cf9596ea6cb29bbb4474da4bdd5a
dtran44 <146193635+dtran44@users.noreply.github.com>
2024-02-12 19:53:25 +1100

    Update activityRoute.js comments

--- controllers/api/activityRoute.js
+++ controllers/api/activityRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const City = require('../../models/City');
 const Amadeus = require('amadeus')
 
+//Finds the selected city by the user
 router.get('/:tempCity', async (req, res) => {
   //   console.log('Accessed the activity route');
   try {
@@ -12,12 +13,14 @@ router.get('/:tempCity', async (req, res) => {
     })
     const cityInfo = cityData.get({plain:true})
     console.log(cityInfo)
-
+    
+//Initialises Amadeus API with client ID and client secret
   const amadeusData =  new Amadeus({
       clientId: process.env.CLIENT_ID, 
       clientSecret: process.env.CLIENT_SECRET,
     })
-   
+    
+// Amadeus API returns the activities based on the city coordinates 
    const amadeusReturn =  await amadeusData.shopping.activities.bySquare.get({
      north: cityInfo.latitudeNorth,
       west: cityInfo.latitudeWest, 
